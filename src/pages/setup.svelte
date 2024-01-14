39a71c53740cba8ef25027d6205a1812536e239a
jaron94 <jonathan.aron94@gmail.com>
2024-01-14 15:55:06 +0000

    Remove game info from setup page

--- src/pages/setup.svelte
+++ src/pages/setup.svelte
@@ -7,7 +7,6 @@
     Block,
   } from "framework7-svelte";
   import Apinput_div from "../components/apinput_div.svelte";
-  import {game} from "../js/store.js";
 </script>
 
 <Page name="home">
@@ -17,10 +16,6 @@
       <Link iconIos="f7:menu" iconMd="material:menu" panelOpen="right" />
     </NavRight>
   </Navbar>
-
-  <!-- Page content -->
-  <h3>Game ID: {$game.id}</h3>
-  <h3>Players: {$game.playerNames}</h3>
 
   <Block>
     <Apinput_div />
