bfacaf52c17f969e89d48f724afdd7ef8e7e6688
jaron94 <jonathan.aron94@gmail.com>
2024-01-18 17:45:41 +0000

    Restyle vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,12 @@
+import path from "path";
 
-import path from 'path';
-
-
-
-const SRC_DIR = path.resolve(__dirname, './src');
-const PUBLIC_DIR = path.resolve(__dirname, './public');
-const BUILD_DIR = path.resolve(__dirname, './www',);
+const SRC_DIR = path.resolve(__dirname, "./src");
+const PUBLIC_DIR = path.resolve(__dirname, "./public");
+const BUILD_DIR = path.resolve(__dirname, "./www");
 export default async () => {
-  const { svelte } = await import('@sveltejs/vite-plugin-svelte');
-  return  {
-    plugins: [
-      svelte(),
-
-    ],
+  const { svelte } = await import("@sveltejs/vite-plugin-svelte");
+  return {
+    plugins: [svelte()],
     root: SRC_DIR,
     base: process.env.NODE_ENV === "production" ? "/bgscorer-svelte/" : "/",
     publicDir: PUBLIC_DIR,
@@ -26,12 +20,11 @@ export default async () => {
     },
     resolve: {
       alias: {
-        '@': SRC_DIR,
+        "@": SRC_DIR,
       },
     },
     server: {
       host: true,
     },
-
   };
-}
+};
