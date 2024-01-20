aea8e4b96a61714a52da75084bed651c7b63d17d
jaron94 <jonathan.aron94@gmail.com>
2024-01-20 12:25:18 +0000

    Remove extraneous pages

--- src/js/routes.js
+++ src/js/routes.js
@@ -1,13 +1,7 @@
 import SetupPage from "../pages/setup.svelte";
 import PlayPage from "../pages/play.svelte";
 import ScorePage from "../pages/score.svelte";
-import AboutPage from "../pages/about.svelte";
-import FormPage from "../pages/form.svelte";
-import ProductPage from "../pages/product.svelte";
-import SettingsPage from "../pages/settings.svelte";
 
-import DynamicRoutePage from "../pages/dynamic-route.svelte";
-import RequestAndLoad from "../pages/request-and-load.svelte";
 import NotFoundPage from "../pages/404.svelte";
 
 var routes = [
@@ -22,71 +16,6 @@ var routes = [
   {
     path: "/score/",
     component: ScorePage,
-  },
-  {
-    path: "/about/",
-    component: AboutPage,
-  },
-  {
-    path: "/form/",
-    component: FormPage,
-  },
-  {
-    path: "/product/:id/",
-    component: ProductPage,
-  },
-  {
-    path: "/settings/",
-    component: SettingsPage,
-  },
-
-  {
-    path: "/dynamic-route/blog/:blogId/post/:postId/",
-    component: DynamicRoutePage,
-  },
-  {
-    path: "/request-and-load/user/:userId/",
-    async: function ({ router, resolve }) {
-      // App instance
-      var app = router.app;
-
-      // Show Preloader
-      app.preloader.show();
-
-      // Simulate Ajax Request
-      setTimeout(function () {
-        // We got user data from request
-        var user = {
-          firstName: "Vladimir",
-          lastName: "Kharlampidi",
-          about: "Hello, i am creator of Framework7! Hope you like it!",
-          links: [
-            {
-              title: "Framework7 Website",
-              url: "http://framework7.io",
-            },
-            {
-              title: "Framework7 Forum",
-              url: "http://forum.framework7.io",
-            },
-          ],
-        };
-        // Hide Preloader
-        app.preloader.hide();
-
-        // Resolve route to load page
-        resolve(
-          {
-            component: RequestAndLoad,
-          },
-          {
-            props: {
-              user: user,
-            },
-          }
-        );
-      }, 1000);
-    },
   },
   {
     path: "(.*)",
